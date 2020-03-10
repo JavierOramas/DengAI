f25dc4d084e3f564f61040ed1045cd83ec8a758d
Javier Oramas <javiale2000@gmail.com>
2020-03-10 00:34:47 -0400

    optimizados hiperparametros, datos de si es pico o no guardado

---
+++ add_spike_data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+df = pd.read_csv('sj_y.csv')
+
+sum = 0
+for i in df['total_cases']:
+    sum = sum + int(i)
+
+sum = sum / len(df['total_cases'])
+print(sum)
+
+bool_list = df['total_cases']-sum > 15
+int_data = [int(i) for i in bool_list]
+
+
+df_x = pd.read_csv('sj.csv')
+
+df_x['is_spike'] = int_data
+df_x.to_csv('sj_spike.csv') 
+
+df = pd.read_csv('iq_y.csv')
+
+sum = 0
+for i in df['total_cases']:
+    sum = sum + int(i)
+
+sum = sum / len(df['total_cases'])
+print(sum)
+
+bool_list = df['total_cases']-sum > 15
+int_data2 = [int(i) for i in bool_list]
+
+df_x = pd.read_csv('iq.csv')
+
+df_x['is_spike'] = int_data2
+df_x.to_csv('iq_spike.csv') 
+
+
+df = pd.read_csv('dengue_features_train.csv')
+df['is_spike'] = int_data+int_data2
+df.to_csv('dengue_features_train_spike.csv')
